38e4f79b817f02b55eb5f9064151f2223f5e923a
Abhinav Kumar <virusfuf@gmail.com>
2023-10-06 12:59:50 +0530

    Update employeeSchema.js

--- models/employeeSchema.js
+++ models/employeeSchema.js
@@ -1,12 +1,32 @@
 const mongoose = require("mongoose");
 
 const employeeSchema = new mongoose.Schema({
+  
+  
   name: String,
-  contactNumber: Number,
-  username: String,
-  email: { type: String, unique: true },
-  password: String,
   accountantId: String,
+  businessName: String,
+  contactNumber: Number,
+  address: {
+    streetName: String,
+    landmark: String,
+    buildingNameNumber: String,
+    postalCode: String,
+  },
+  vatNumber: Number,
+  crnNumber: Number,
+  banks: [
+    {
+      bankName: String,
+      accountName: String,
+      accountNumber: String,
+      sortCode: String,
+    },
+  ],
+  logo: {type:String}, // Assuming logo links are strings
+  username: { type: String, unique: true },
+  email: { type: String, unique:true },
+  password: String,
 });
 
 module.exports = mongoose.model("Employee", employeeSchema);
